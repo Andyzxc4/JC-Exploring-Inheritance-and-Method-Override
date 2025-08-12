c9b0a0d14a58c77dd9b3e6bf4ea76a74222523ef
Andyzxc4 <lacraandre111@gmail.com>
2025-08-12 14:29:52 +0800

    feat: initialize file structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //  1. Basic Inheritance: The extends Keyword
+        
+
+        //  2. Abstract Classes: Defining a Template
+
+
+        //  3. Method Overriding: Providing a New Implementation
+
+
+        //  4. Inheriting Variables & Access Rules
+
+    }
+}
